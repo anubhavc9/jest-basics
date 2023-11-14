033545abbb71987cc732d9fcd02a9f7dbba31d75
Anubhav Kandiyal <anubhav.kandiyal@agrostar.in>
2023-11-14 17:39:59 +0530

    chunk an array function test cases added

---
+++ chunkArray.js
@@ -0,0 +1,21 @@
+// chunkArray([1,2,3,4,5,6,7],3) = [[1,2,3],[4,5,6],[7]]
+
+const chunkArray = (arr, len) => {
+  const chunkedArr = [];
+
+  arr.forEach((val) => {
+    // Get last element
+    const last = chunkedArr[chunkedArr.length - 1];
+
+    // Check if last and if last length is equal to the chunk len
+    if (!last || last.length === len) {
+      chunkedArr.push([val]);
+    } else {
+      last.push(val);
+    }
+  });
+
+  return chunkedArr;
+};
+
+module.exports = chunkArray;
