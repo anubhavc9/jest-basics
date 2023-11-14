fc4e377451577e3988a11df72837f4eaafafb588
Anubhav Kandiyal <anubhav.kandiyal@agrostar.in>
2023-11-14 23:45:47 +0530

    formatting made worse

--- functions.js
+++ functions.js
@@ -13,6 +13,6 @@ fetchUser: () =>
     axios
       .get("https://jsonplaceholder.typicode.com/users/1")
       .then((res) => res.data)
-      .catch((err) => "error"),
+      .catch((err) => "error")
 }
 module.exports = functions;
