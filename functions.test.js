6558aa12ff80e463592e43fb961bb7e1eae0e46a
Anubhav Kandiyal <anubhav.kandiyal@agrostar.in>
2023-11-14 14:44:35 +0530

    more matchers

--- functions.test.js
+++ functions.test.js
@@ -54,3 +54,11 @@ test("User should be Anubhav Kandiyal object", () => {
 });
 
 // *************************************************************** //
+
+// OTHER AVAILABLE MATCHERS: toBeLessThan, toBeLessThanOrEqual
+test("Should be under 1600", () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1600);
+});
+// *************************************************************** //
